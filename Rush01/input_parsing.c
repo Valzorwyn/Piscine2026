8ffff09760c285c94549e21f58e32deadc6f9051
Balint Jankovics <bajankov@c2r1p9.42vienna.com>
2026-03-14 19:05:33 +0100

    asd

--- Rush01/input_parsing.c
+++ Rush01/input_parsing.c
@@ -1,22 +1,28 @@
+#include <stdio.h>
+
 int is_num(char c);
 
 int gridsize(int arr[46]);
+
+int check_format(char *arr);
 
 int is_input_correct(int ac, char *av[])
 {
     int all[46];
     int i;
+    int a;
 
-    if (ac != 2)
+    if (ac != 2 || check_format(av[ac - 1]) != 1)
         return (0);
     i = 0;
     while (i < 46)
         all[i++] = 0;
     i = 0;
+    a = 0;
     while (av[ac - 1][i])
     {
         if (is_num(av[ac - 1][i]))
-            all[i] = av[ac - 1][i] - '0';
+            all[a++] = av[ac - 1][i] - '0';
         else if (av[ac - 1][i] != ' ')
             return (0);
         i++;
@@ -24,27 +30,57 @@ int is_input_correct(int ac, char *av[])
     return (gridsize(all));
 }
 
+int check_format(char *arr)
+{
+    int i;
+
+    i = 0;
+    while (arr[i])
+    {
+        if ((i + 1) % 2 == 0)
+            if (arr[i] != ' ')
+                return (0);
+        i++;
+    }
+    if (is_num(arr[i - 1]))
+        return (1);
+    return (0);
+}
+
 int gridsize(int arr[46])
 {
-    int i = 0;
+    int i;
+    int j;
     int size;
+
+    i = 0;
+    j = 0;
     while (i < 46 && arr[i])
         i++;
     size = 9;
     while (size >= 4)
     {
-        if (i % size == 0)
+        if ((i / 4) % size == 0 && (i % 4) == 0)
+        {
+            while (j < 46)
+                if (arr[j++] > size)
+                    return (0);
             return (size);
-        else
-            return (0);
+        }
         size--;
     }
+    return (0);
 }
 
 int is_num(char c)
 {
-    if (c < '0' || c > '9')
+    if (c < '1' || c > '9')
         return (0);
     else
         return (1);
+}
+
+int main(int argc, char *argv[])
+{
+    printf("\n%d", is_input_correct(argc, argv));
 }
---
+++ Rush01/main.c
@@ -0,0 +1,6 @@
+int	main(int argc, char **argv)
+{
+	if(!input_parsing(argc, argv) || argc != 2);
+		return (write(2, "Error\n", 6), 1);
+	
+}
