ae147a09bdb272025de0dae48c86b2ce3b8e3657
Balint Jankovics <bajankov@c2r1p9.42vienna.com>
2026-03-22 15:33:23 +0100

    strjoinfix again

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bajankov <bajankov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 17:44:49 by bajankov          #+#    #+#             */
-/*   Updated: 2026/03/19 18:50:22 by bajankov         ###   ########.fr       */
+/*   Updated: 2026/03/22 15:31:47 by bajankov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,53 +82,24 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	return (res);
 }
 
-char	*ft_strjoin(int size, char **strs, char *sep)
-{
-	char	*str;
-	int		len;
-	int		i;
+// #include <stdio.h>
+// int main(int argc, char **argv)
+// {
+//     char    *result;
 
-	if (size == 0)
-		return (ft_calloc(1, 1));
-	if (size < 0)
-		return (NULL);
-	i = -1;
-	len = 0;
-	while (++i < size)
-		len += ft_strlen(strs[i]);
-	len += (size - 1) * ft_strlen(sep);
-	str = (char *)ft_calloc(1, 1 + len);
-	if (!str)
-		return (NULL);
-	i = -1;
-	while (++i < size)
-	{
-		ft_strcat(str, strs[i]);
-		if (i < size - 1)
-			ft_strcat(str, sep);
-	}
-	return (str);
-}
+//         // argv[1] = separator
+//         // argv[2..] = strings
+//         result = ft_strjoin(argc - 2, &argv[2], argv[1]);
 
+//     if (!result)
+//     {
+//         printf("Memory allocation failed\n");
+//         return (1);
+//     }
 
-#include <stdio.h>
-int main(int argc, char **argv)
-{
-    char    *result;
+//     printf("Separator: \"%s\"\n", argv[1]);
+//     printf("Result: \"%s\"\n", result);
 
-        // argv[1] = separator
-        // argv[2..] = strings
-        result = ft_strjoin(argc - 2, &argv[2], argv[1]);
-
-    if (!result)
-    {
-        printf("Memory allocation failed\n");
-        return (1);
-    }
-
-    printf("Separator: \"%s\"\n", argv[1]);
-    printf("Result: \"%s\"\n", result);
-
-    free(result);
-    return (0);
-}
+//     free(result);
+//     return (0);
+// }
